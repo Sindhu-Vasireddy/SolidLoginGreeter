0e53bf4d3d956e770b05a9487d520725f391f917
Sindhu-Vasireddy <98752594+Sindhu-Vasireddy@users.noreply.github.com>
2022-06-23 17:40:35 +0100

    Updated Source

--- main.js
+++ main.js
@@ -28,12 +28,59 @@ async function handleRedirectAfterLogin() {
        await handleIncomingRedirect();
               
        if(getDefaultSession().info.isLoggedIn){
+            console.log("I'm inside!!!!");
             let data_array=await getDataFromWebID(window.sessionStorage.getItem('webID_later'));
-            document.getElementById('webid_Login').classList.add("hidden");
-            document.getElementById('webid').classList.add("hidden");
-            document.getElementById('output').textContent="Hi "+ data_array[0]+" "+data_array[1];
 
-         
+            if(data_array[0]==window.sessionStorage.getItem('webID_later')){
+                document.getElementById('Name').classList.remove("hidden")
+                document.getElementById('output_name').textContent="Would you like to be called by your name instead!?";
+                // Get the input field
+                var input = document.getElementById("Name");
+
+                // Execute a function when the user presses a key on the keyboard
+                input.addEventListener("keypress", async function(event) {
+                  // If the user presses the "Enter" key on the keyboard
+                  if (event.key === "Enter") {
+                    // Cancel the default action, if needed
+                    event.preventDefault();
+
+                    const query_extra= `INSERT DATA {:me <http://xmlns.com/foaf/0.1/Name> "${document.getElementById('Name').value}".}`;
+                
+                    // Send a GET and PUT request to update the source
+                    const query = query_extra;
+                    const response_put = await solidfetch(window.sessionStorage.getItem('webID_later'), {
+                    method: 'PATCH',
+                    headers: { 'Content-Type': 'application/sparql-update' },
+                    body: query,
+                    credentials:'include'
+                    });
+                     if(response_put.status>=200&&response_put.status<=300){
+                        document.getElementById('Name').classList.add("hidden");
+                        document.getElementById('output_name').classList.add("hidden");
+
+                        let data_array=await getDataFromWebID(window.sessionStorage.getItem('webID_later'));
+                        console.log(data_array);   
+                        document.getElementById('output').textContent="Hi "+ data_array[0]+" "+data_array[1]+"!";                     
+                    }
+                }
+                  
+
+                });
+
+
+
+            
+        }
+
+
+
+            document.getElementById('output').textContent="Hi "+ data_array[0]+" "+data_array[1]+"!";
+            document.getElementById('webid').classList.add("hidden");
+        
+
+            console.log(document.getElementById('webid'));
+            document.getElementById('webid_Login').classList.add("hidden");
+            console.log(document.getElementById('webid_Login'));
         }
 }
 
@@ -45,6 +92,14 @@ document.getElementById('webid_Login').addEventListener('click', async () => { l
     document.getElementById('output').textContent="OIDC Issuer not found in webid or invalid webID."+error;
   }} );
 
+async function myfetchFunction(url){
+  return await solidfetch(url, {
+       method: 'GET',
+       headers: { 'Content-Type': 'application/sparql-update','Cache-Control': 'no-cache' },
+       credentials: 'include'
+        });
+}
+
 async function getIssuerFromWebID(webid){
   var myEngine_getIssuer = new QueryEngine();
 
@@ -53,7 +108,9 @@ async function getIssuerFromWebID(webid){
    ?s <http://www.w3.org/ns/solid/terms#oidcIssuer> ?o.
   }`, {
     sources: [`${webid}`],
+    fetch: myfetchFunction,
   });
+  myEngine_getIssuer.invalidateHttpCache();
   const bindings = await bindingsStream.toArray();
   // console.log(`${bindings[0].get('o').value}`);
   return(bindings[0].get('o').value); 
@@ -68,11 +125,32 @@ async function getDataFromWebID(webid){
   <http://xmlns.com/foaf/0.1/givenName> ?givenName.
   }`, {
     sources: [`${webid}`],
+    fetch: myfetchFunction,
   });
+  myEngine_getData.invalidateHttpCache();
     const bindings = await bindingsStream.toArray();
-     if(bindings.length==0){ //When the foaf:givenName and foaf:FamilyName is absent webid will be shown
-        return([webid,'']) }
-     else{
+     if(bindings.length==0){ //When the foaf:givenName and foaf:FamilyName is absent but foaf:name is present
+          var myEngine_getData_ = new QueryEngine();
+          const bindingsStream_ = await myEngine_getData_.queryBindings(`
+          SELECT ?Name  WHERE {
+           ?s <http://xmlns.com/foaf/0.1/Name> ?Name.
+          }`, {
+            sources: [`${webid}`],
+            fetch: myfetchFunction,
+          });
+          myEngine_getData_.invalidateHttpCache();
+        const bindings_ = await bindingsStream_.toArray();
+            
+        if(bindings_.length==0){ 
+        //When the foaf:givenName, foaf:Name and foaf:FamilyName are absent webid will be shown
+            return([webid,'']) }
+
+        else{
+            return([bindings_[0].get('Name').value,""]);
+        } 
+
+    }
+        else{
             return([bindings[0].get('givenName').value,bindings[0].get('familyName').value]);
          }
 }
